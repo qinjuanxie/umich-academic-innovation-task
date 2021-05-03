cfe0ccd8f0e20415e33b36e3b4be6aaa6504da88
qinjuanxie <qinjuanx@umich.edu>
2021-05-03 00:31:25 -0400

    fundamental funcs done. Todo: Edge cases & CSS

--- main.py
+++ main.py
@@ -5,31 +5,20 @@ app = flask.Flask(__name__)
 
 from flask import session
 import requests
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+from io import StringIO
+from io import BytesIO
+import base64
 
 @app.route('/', methods=['GET', 'POST'])
 def show_main():
-    flickr_key = "d57e81fd7e74e169f325dff9c089283a"
-    # if flask.request.method == 'POST':
-    baseurl = "https://www.flickr.com/services/rest/"
-    params_dict = {}
-    params_dict["method"] = "flickr.photos.search"
-    params_dict["api_key"] = flickr_key
-    params_dict["tags"] = "rivers"
-    params_dict["format"] = "json"
-    params_dict["nojsoncallback"] = 1
-    flickr_r = requests.get(baseurl, params = params_dict)
-    print(flickr_r.url)
-    data_json = flickr_r.json()["photos"]["photo"]
-    context = {
-        "photo": data_json
-    }
-
-    return flask.render_template("main.html", **context)
+    return flask.render_template("main.html")
 
 @app.route('/results', methods=['GET', 'POST'])
 def show_results():
     if flask.request.method == 'POST':
-
         flickr_key = "d57e81fd7e74e169f325dff9c089283a"
         baseurl = "https://www.flickr.com/services/rest/"
         params_dict = {}
@@ -53,3 +42,28 @@ def show_results():
         }
 
         return flask.render_template("results.html", **context)
+
+@app.route('/selected', methods=['GET', 'POST'])
+def show_selection():
+    if "input-text" in flask.request.form:
+        # Open an Image
+        img = Image.open(BytesIO(requests.get(flask.request.form["img_url"]).content))
+
+        # Call draw Method to add 2D graphis in an image
+        I1 = ImageDraw.Draw(img)
+        
+        # Cutom font style and font size
+        myFont = ImageFont.truetype('FreeMono.ttf', 65)
+
+        # Add Text to an image
+        I1.text((10, 10), flask.request.form["input-text"], font=myFont, fill =(0, 0, 0))
+
+        output = BytesIO()
+        img.save(output, "PNG")
+        contents = base64.b64encode(output.getvalue())
+
+        return flask.render_template("selected.html", old_img_url=flask.request.form["img_url"], img_data=contents.decode('utf-8'))
+
+    else:
+        return flask.render_template("selected.html", old_img_url=flask.request.form["img_url"])
+
