ef29f09e1c2a60d70982e2eeebabb5be0a62ad8f
qinjuanxie <qinjuanx@umich.edu>
2021-05-03 14:26:31 -0400

    CSS updated. Required text added.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from PIL import ImageFont
 from io import StringIO
 from io import BytesIO
 import base64
+import textwrap
 
 @app.route('/', methods=['GET', 'POST'])
 def show_main():
@@ -18,30 +19,29 @@ def show_main():
 
 @app.route('/results', methods=['GET', 'POST'])
 def show_results():
-    if flask.request.method == 'POST':
-        flickr_key = "d57e81fd7e74e169f325dff9c089283a"
-        baseurl = "https://www.flickr.com/services/rest/"
-        params_dict = {}
-        params_dict["method"] = "flickr.photos.search"
-        params_dict["api_key"] = flickr_key
-        params_dict["tags"] = flask.request.form["tags"]
-        params_dict["format"] = "json"
-        params_dict["nojsoncallback"] = 1
-        flickr_r = requests.get(baseurl, params = params_dict)
-        print(flickr_r.url)
-        data_json = flickr_r.json()["photos"]["photo"]
-        
-        urls = []
+    flickr_key = "d57e81fd7e74e169f325dff9c089283a"
+    baseurl = "https://www.flickr.com/services/rest/"
+    params_dict = {}
+    params_dict["method"] = "flickr.photos.search"
+    params_dict["api_key"] = flickr_key
+    params_dict["tags"] = flask.request.form["tags"]
+    params_dict["format"] = "json"
+    params_dict["nojsoncallback"] = 1
+    flickr_r = requests.get(baseurl, params = params_dict)
+    data_json = flickr_r.json()["photos"]["photo"]
+    
+    urls = []
 
-        for i in range(0, len(data_json)):
-            url = "http://farm"+str(data_json[i]["farm"])+".staticflickr.com/"+str(data_json[i]["server"])+"/"+str(data_json[i]["id"])+"_"+str(data_json[i]["secret"])+".jpg"
-            urls.append(url)
-        
-        context = {
-            "photo": urls
-        }
+    for i in range(0, len(data_json)):
+        url = "http://farm"+str(data_json[i]["farm"])+".staticflickr.com/"+str(data_json[i]["server"])+"/"+str(data_json[i]["id"])+"_"+str(data_json[i]["secret"])+".jpg"
+        urls.append(url)
+    
+    context = {
+        "size": len(urls),
+        "photo": urls
+    }
 
-        return flask.render_template("results.html", **context)
+    return flask.render_template("results.html", **context)
 
 @app.route('/selected', methods=['GET', 'POST'])
 def show_selection():
@@ -50,13 +50,20 @@ def show_selection():
         img = Image.open(BytesIO(requests.get(flask.request.form["img_url"]).content))
 
         # Call draw Method to add 2D graphis in an image
-        I1 = ImageDraw.Draw(img)
+        draw = ImageDraw.Draw(img)
         
         # Cutom font style and font size
-        myFont = ImageFont.truetype('FreeMono.ttf', 65)
+        myFont = ImageFont.truetype('FreeMono.ttf', 30)
 
         # Add Text to an image
-        I1.text((10, 10), flask.request.form["input-text"], font=myFont, fill =(0, 0, 0))
+        # Wrap it if too long
+        lines = textwrap.wrap(text=flask.request.form["input-text"], width=20)
+        img_w,img_h = img.size
+        y_text = 0
+        for line in lines:
+            line_w, line_h = myFont.getsize(line)
+            draw.text(((img_w - line_w) / 2, y_text), line, font=myFont, fill=(255,203,5))
+            y_text += line_h
 
         output = BytesIO()
         img.save(output, "PNG")
